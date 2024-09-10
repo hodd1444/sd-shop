d62ac3afff8e5cc8d3a612c1064ad5ad6bd1b80c
sm <entertainmenthodd@gmail.com>
2024-09-10 22:32:21 +0200

    Added wasabi bundle general structure to items

--- components/BundleViewer.tsx
+++ components/BundleViewer.tsx
@@ -40,52 +40,55 @@ export default function BundleViewer({ bundle }: BundleViewerProps) {
             {bundle.price}
           </p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {skins.map((skin) => (
-            <Link
-              href={`/item/${skin.name.toLowerCase().replace(/ /g, "-")}`}
-              key={skin.id}
-            >
-              <div className="bg-[#333333] rounded-lg overflow-hidden shadow-lg transition-transform hover:scale-105">
-                <img
-                  src={skin.image}
-                  alt={skin.name}
-                  className="w-full h-48 object-cover"
-                />
-                <div className="p-4">
-                  <h3 className="text-xl font-semibold text-[#FFCB00] mb-4">
-                    {skin.name}
-                  </h3>
-                  {skin.price === "bundle" ? (
-                    <p className="flex flex-row text-[#bdbdbd] mb-2">
-                      <img
-                        className="w-6 h-6 mr-2"
-                        src="/spectre_points_gray.webp"
-                      />
-                      Bundle only
+        {skins.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {skins.map((skin) => (
+              <Link
+                href={`/item/${skin.name.toLowerCase().replace(/ /g, "-")}`}
+                key={skin.id}
+              >
+                <div className="bg-[#333333] rounded-lg overflow-hidden shadow-lg transition-transform hover:scale-105">
+                  <img
+                    src={skin.image}
+                    alt={skin.name}
+                    className="w-full h-48 object-cover"
+                  />
+                  <div className="p-4">
+                    <h3 className="text-xl font-semibold text-[#FFCB00] mb-4">
+                      {skin.name}
+                    </h3>
+                    {skin.price === "bundle" ? (
+                      <p className="flex flex-row text-[#bdbdbd] mb-2">
+                        <img
+                          className="w-6 h-6 mr-2"
+                          src="/spectre_points_gray.webp"
+                        />
+                        Bundle only
+                      </p>
+                    ) : (
+                      <p className="flex flex-row text-[#EC3C7C] mb-2">
+                        <img
+                          className="w-6 h-6 mr-2"
+                          src="/spectre_points_pink.webp"
+                        />
+                        {skin.price}
+                      </p>
+                    )}
+                    <p
+                      className="text-sm font-bold text-gray-800 rounded-md w-fit px-2 py-1"
+                      style={{
+                        backgroundColor: skin.color,
+                      }}
+                    >
+                      {skin.rarity}
                     </p>
-                  ) : (
-                    <p className="flex flex-row text-[#EC3C7C] mb-2">
-                      <img
-                        className="w-6 h-6 mr-2"
-                        src="/spectre_points_pink.webp"
-                      />
-                      {skin.price}
-                    </p>
-                  )}
-                  <p
-                    className="text-sm font-bold text-gray-800 rounded-md w-fit px-2 py-1"
-                    style={{
-                      backgroundColor: skin.color,
-                    }}
-                  >
-                    {skin.rarity}
-                  </p>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          ))}
-        </div>
+              </Link>
+            ))}
+          </div>) : (
+          <p className="text-xl text-center">No items to show right now! Check back later 👻</p>
+          )}
       </div>
     </div>
   );
