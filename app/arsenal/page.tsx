2802fc2b5791694e69cbbdd2a0810f0bef010be7
sm <entertainmenthodd@gmail.com>
2024-09-13 03:35:50 +0200

    og fix maybe idk

--- app/arsenal/page.tsx
+++ app/arsenal/page.tsx
@@ -1,17 +1,5 @@
 import WeaponList from "@/components/WeaponList";
 import Header from "@/components/Header";
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Arsenal | Spectre',
-  description: 'Browse all available weapons in Spectre Divide and explore their stats, prices, and bundles.',
-  keywords: 'weapons, gun list, weapon stats, Spectre Divide',
-  openGraph: {
-    title: 'Arsenal | Spectre',
-    description: 'Browse all available weapons in Spectre Divide and explore their stats, prices, and bundles.',
-    images: '/opengraph-image.png',
-  },
-};
 
 export default function WeaponListPage() {
     return (
