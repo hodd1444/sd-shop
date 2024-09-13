61e07af397683c56ac543cf470672fb58709b392
sm <entertainmenthodd@gmail.com>
2024-09-13 03:28:08 +0200

    idk how opengraph works

--- app/arsenal/page.tsx
+++ app/arsenal/page.tsx
@@ -2,15 +2,15 @@ import WeaponList from "@/components/WeaponList";
 import Header from "@/components/Header";
 import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Arsenal | Spectre',
-  description: 'Browse all available weapons in Spectre Divide and explore their stats, prices, and bundles.',
-  keywords: 'weapons, gun list, weapon stats, Spectre Divide',
-};
+
 
 export default function WeaponListPage() {
     return (
         <div>
+            <meta property="og:image" content="<generated>" />
+            <meta property="og:title" content="Arsenal | Spectre" />
+            <meta property="og:description" content="Browse all available weapons in Spectre Divide and explore their stats, prices, and bundles." />
+            <meta property="og:keywords" content="weapons, gun list, weapon stats, Spectre Divide" />
             <Header />
             <WeaponList />
         </div>
