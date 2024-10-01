ccc64452ecb40e376ac0d0bafd8e39e1c1f5527b
sm <entertainmenthodd@gmail.com>
2024-10-01 22:30:47 +0200

    Oct1 shop update

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -6,9 +6,9 @@ import { BundleBannerCard } from "./BundleBannerCard";
 const bundles1 = [
   {
     id: 1,
-    name: "Cyberlord Bundle",
-    price: "4,800",
-    image: "/shop_091724/cyberlord_bundle.webp",
+    name: "Alchemist Bundle",
+    price: "7,000",
+    image: "/shop_100124/alchemist_bundle.webp",
     imageSet: [],
     rarity: "red",
     color: "#EA3546",
@@ -16,31 +16,6 @@ const bundles1 = [
 ]
 
 const bundles2 = [
-  {
-    id: 1,
-    name: "Cyberlord",
-    price: "1,500",
-    image: "/shop_091724/cyberlord.webp",
-    imageSet: [
-      {
-        id: 0,
-        src: "/shop_091724/cyberlord_splashart/BG.webp",
-      },
-      {
-        id: 1,
-        src: "/shop_091724/cyberlord_splashart/1.webp",
-      },
-      {
-        id: 2,
-        src: "/shop_091724/cyberlord_splashart/2.webp",
-      },
-    ],
-    rarity: "red",
-    color: "#EA3546",
-  }
-]
-
-const bundles3 = [
   {
     id: 1,
     name: "Hopscotch",
@@ -60,81 +35,6 @@ const bundles3 = [
     color: "#F9B428",
   }
 ]
-
-const oldBundles2 = [
-  {
-    id: 1,
-    name: "Cryo Kinesis Bundle",
-    price: "7,000",
-    image: "/shop_090324/cryo_kinesis_bundle.webp",
-    imageSet: [
-      {
-        id: 0,
-        src: "/shop_090324/cryo_kinesis_bundle_splashart/BG.webp",
-      },
-      {
-        id: 1,
-        src: "/shop_090324/cryo_kinesis_bundle_splashart/1.webp",
-      },
-      {
-        id: 2,
-        src: "/shop_090324/cryo_kinesis_bundle_splashart/2.webp",
-      },
-      {
-        id: 3,
-        src: "/shop_090324/cryo_kinesis_bundle_splashart/3.webp",
-      },
-      {
-        id: 4,
-        src: "/shop_090324/cryo_kinesis_bundle_splashart/4.webp",
-      },
-      {
-        id: 5,
-        src: "/shop_090324/cryo_kinesis_bundle_splashart/5.webp",
-      },
-      {
-        id: 6,
-        src: "/shop_090324/cryo_kinesis_bundle_splashart/6.webp",
-      },
-    ],
-    rarity: "red",
-    color: "#EA3546",
-  },
-  {
-    id: 2,
-    name: "Medusa Bundle",
-    price: "3,400",
-    image: "/shop_090324/medusa_bundle.webp",
-    imageSet: [
-      {
-        id: 0,
-        src: "/shop_090324/medusa_bundle_splashart/BG.webp",
-      },
-      {
-        id: 1,
-        src: "/shop_090324/medusa_bundle_splashart/1.webp",
-      },
-      {
-        id: 2,
-        src: "/shop_090324/medusa_bundle_splashart/2.webp",
-      },
-      {
-        id: 3,
-        src: "/shop_090324/medusa_bundle_splashart/3.webp",
-      },
-      {
-        id: 4,
-        src: "/shop_090324/medusa_bundle_splashart/4.webp",
-      },
-      {
-        id: 5,
-        src: "/shop_090324/medusa_bundle_splashart/5.webp",
-      },
-    ],
-    rarity: "orange",
-    color: "#F9B428",
-  },
-];
 
 const spotlight1 = [
   {
@@ -209,92 +109,33 @@ const offers1 = [
   },
   {
     id: 2,
-    name: "Outrider",
-    price: "1,500",
-    image: "/shop_090324/outrider.webp",
-    rarity: "red",
-    color: "#EA3546",
+    name: "Eternal Warrior Crusader",
+    price: "1,200",
+    image: "/shop_091724/eternal_warrior_crusader.webp",
+    rarity: "orange",
+    color: "#F9B428",
   },
   {
     id: 3,
-    name: "Prince",
-    price: "1,500",
-    image: "/shop_090324/prince.webp",
-    rarity: "red",
-    color: "#EA3546",
+    name: "Troublemaker Cyclone",
+    price: "800",
+    image: "/shop_091724/troublemaker_cyclone.webp",
+    rarity: "purple",
+    color: "#B661FF",
   },
   {
     id: 4,
-    name: "Eternal Warrior M49 Fury",
-    price: "1,200",
-    image: "/shop_090324/eternal_warrior_m49_fury.webp",
-    rarity: "orange",
-    color: "#F9B428",
+    name: "Troublemaker M18 Drummer",
+    price: "800",
+    image: "/shop_091724/troublemaker_m18_drummer.webp",
+    rarity: "purple",
+    color: "#B661FF",
   },
 ];
 
 const offers2 = [
   {
     id: 1,
-    name: "Mako Buzzsaw RT40",
-    price: "800",
-    image: "/shop_090324/mako_buzzsaw_rt40.webp",
-    rarity: "purple",
-    color: "#B661FF",
-  },
-  {
-    id: 2,
-    name: "Mako Harpe",
-    price: "800",
-    image: "/shop_090324/mako_harpe.webp",
-    rarity: "purple",
-    color: "#B661FF",
-  },
-  {
-    id: 3,
-    name: "Eternal Warrior Duster RX6",
-    price: "1,200",
-    image: "/shop_090324/eternal_warrior_duster_rx6.webp",
-    rarity: "orange",
-    color: "#F9B428",
-  },
-  {
-    id: 4,
-    name: "Elegant Operative",
-    price: "1,500",
-    image: "/shop_090324/elegant_operative.webp",
-    rarity: "red",
-    color: "#EA3546",
-  },
-  {
-    id: 5,
-    name: "High Performance",
-    price: "1,500",
-    image: "/shop_090324/high_performance.webp",
-    rarity: "red",
-    color: "#EA3546",
-  },
-];
-
-const offers3 = [
-  {
-    id: 1,
-    name: "04",
-    price: "1,000",
-    image: "/shop_090324/04.webp",
-    rarity: "orange",
-    color: "#F9B428",
-  },
-  {
-    id: 2,
-    name: "Starbright",
-    price: "1,000",
-    image: "/shop_090324/starbright.webp",
-    rarity: "orange",
-    color: "#F9B428",
-  },
-  {
-    id: 3,
     name: "Primal",
     price: "1,500",
     image: "/shop_090324/primal.webp",
@@ -302,36 +143,84 @@ const offers3 = [
     color: "#EA3546",
   },
   {
+    id: 2,
+    name: "Prince",
+    price: "1,500",
+    image: "/shop_090324/prince.webp",
+    rarity: "red",
+    color: "#EA3546",
+  },
+  {
+    id: 3,
+    name: "Outrider",
+    price: "1,500",
+    image: "/shop_090324/outrider.webp",
+    rarity: "red",
+    color: "#EA3546",
+  },
+  {
     id: 4,
+    name: "04",
+    price: "1,000",
+    image: "/shop_090324/04.webp",
+    rarity: "orange",
+    color: "#F9B428",
+  },
+  {
+    id: 5,
+    name: "Mako Beserker RB3",
+    price: "800",
+    image: "/shop_100124/mako_beserker_rb3.webp",
+    rarity: "purple",
+    color: ""
+  }
+];
+
+const offers3 = [
+  {
+    id: 1,
+    name: "High Performance",
+    price: "1,500",
+    image: "/shop_090324/high_performance.webp",
+    rarity: "red",
+    color: "#EA3546",
+  },
+  {
+    id: 2,
+    name: "Elegant Operative",
+    price: "1,500",
+    image: "/shop_090324/elegant_operative.webp",
+    rarity: "red",
+    color: "#EA3546",
+  },
+  {
+    id: 3,
     name: "Popstar",
     price: "1,500",
     image: "/shop_090324/popstar.webp",
     rarity: "red",
     color: "#EA3546",
   },
-];
-
-const offers4 = [
   {
-    id: 1,
+    id: 4,
     name: "Beast",
     price: "1,000",
     image: "/shop_090324/beast.webp",
     rarity: "orange",
-    color: "#F9B428",
+    color:"#F9B428"
   },
   {
-    id: 2,
-    name: "Bloomtech Outfitters",
-    price: "1,000",
-    image: "/shop_090324/bloomtech_outfitters.webp",
+    id: 5,
+    name: "Eternal Warrior Harpe",
+    price: "800",
+    image: "/shop_100124/eternal_warrior_harpe.webp",
     rarity: "orange",
     color: "#F9B428",
   },
 ];
 
 export default function Shop() {
-  const [offerTimeLeft, setShopTimeLeft] = useState("23:59:59");
+  const [offerTimeLeft, setOfferTimeLeft] = useState("23:59:59");
   const [featured1TimeLeft, setFeatured1TimeLeft] = useState("23:59:59");
   const [featured2TimeLeft, setFeatured2TimeLeft] = useState("23:59:59");
   const [spotlight1TimeLeft, setSpotlight1TimeLeft] = useState("23:59:59");
@@ -339,11 +228,11 @@ export default function Shop() {
 
   const shopAvailable = true;
   useEffect(() => {
-    const shopResetDate = new Date(Date.UTC(2024, 9, 1, 20, 0, 0, 0)); // October is month 9 (0-indexed)
+    const offerResetDate = new Date(Date.UTC(2024, 9, 15, 20, 0, 0, 0)); // October is month 9 (0-indexed)
 
     const updateCountdown = () => {
       const now = new Date();
-      const timeDiff = shopResetDate.getTime() - now.getTime();
+      const timeDiff = offerResetDate.getTime() - now.getTime();
 
       const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
       const hours = Math.floor(
@@ -351,7 +240,7 @@ export default function Shop() {
       );
       const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
 
-      setShopTimeLeft(
+      setOfferTimeLeft(
         `${days.toString()} DAYS ${hours
           .toString()
           .padStart(2, "0")} HOURS ${minutes
@@ -368,7 +257,7 @@ export default function Shop() {
 
   // Featured 1
   useEffect(() => {
-    const featured1ResetDate = new Date(Date.UTC(2024, 8, 31, 20, 0, 0, 0));
+    const featured1ResetDate = new Date(Date.UTC(2024, 9, 15, 20, 0, 0, 0));
 
 
     const updateCountdownFeatured = () => {
@@ -495,24 +384,15 @@ export default function Shop() {
       <div className="max-w-5xl w-full">
         <div className="flex flex-row items-center align-middle mb-4 mt-8 gap-4">
           <h2 className="text-2xl font-semibold">FEATURED</h2>
-          <p className="text-gray-400">{featured2TimeLeft}</p>
-        </div>
-
-        <div className="flex flex-col justify-between gap-8">
-          <BundleBannerCard bundles={bundles3} />
-        </div>
-        <div className="flex flex-row items-center align-middle mb-4 mt-8 gap-4">
-          <h2 className="text-2xl font-semibold">FEATURED</h2>
           <p className="text-gray-400">{featured1TimeLeft}</p>
         </div>
 
         <div className="flex flex-col justify-between gap-8">
           <BundleBannerCard bundles={bundles1} />
         </div>
-
         <div className="flex flex-row items-center align-middle mb-4 mt-8 gap-4">
           <h2 className="text-2xl font-semibold">FEATURED</h2>
-          <p className="text-gray-400">{featured1TimeLeft}</p>
+          <p className="text-gray-400">{featured2TimeLeft}</p>
         </div>
 
         <div className="flex flex-col justify-between gap-8">
@@ -665,37 +545,6 @@ export default function Shop() {
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 mt-8">
           {offers3.map((item) => (
-            <div
-              key={item.id}
-              className="bg-[#333333] rounded-lg overflow-hidden shadow-lg transition-transform hover:scale-105"
-            >
-              <img
-                src={item.image}
-                alt={item.name}
-                className="w-full object-cover"
-              />
-              <div className="p-4">
-                <h3
-                  className={`text-lg font-semibold mb-2`}
-                  style={{
-                    color: item.color,
-                  }}
-                >
-                  {item.name}
-                </h3>
-                <div className="flex items-center text-[#bdbdbd]">
-                  <img
-                    src="/spectre_points_gray.webp"
-                    className="w-5 h-5 mr-1"
-                  />
-                  <span>{item.price}</span>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6 mt-8">
-          {offers4.map((item) => (
             <div
               key={item.id}
               className="bg-[#333333] rounded-lg overflow-hidden shadow-lg transition-transform hover:scale-105"
